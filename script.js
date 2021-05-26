4ece923fbf5cdecaa4d7148e2d1cbf404417c09d
Ncesecrede <francescopm95@gmail.com>
2021-05-26 15:29:16 +0100

    PlayStation

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 document.getElementById("title-one").innerHTML="Ecco la PlayStation 5"
 alert("Clicca ok per visualizzare la pagina")
+console.log("ciao")
+document.getElementById("title-two").innerHTML="Un'avventura cooperativa selvaggia ti attende";
+var x = 250;
+var y = 250;
+var z = x + y;
+document.getElementById("result").innerHTML="+z"
